f31b4a8afe82b097610252866f61cf5f0110ed73
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-08-07 07:13:44 -0300

    Iniciando Mysql no python

--- MySQL/main.py
+++ MySQL/main.py
@@ -8,6 +8,7 @@ MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
+TABLE_NAME = 'users'
 
 connection = pymysql.connect(
     host=MYSQL_HOST,
@@ -18,14 +19,50 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS users ('
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
     'id INT NOT NULL AUTO_INCREMENT, '
     'name VARCHAR(50) NOT NULL, '
-    'age INT NOT NULL, '
+    'age BIGINT NOT NULL, '
     'PRIMARY KEY (id)'
     ')'
 )
+cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # Isso limpa a tabela
 
+connection.commit()
+
+sql = (
+       f'INSERT INTO {TABLE_NAME} '
+       '(name, age) '
+       'VALUES '
+       '(%s, %s) '
+    )
+
+name = input('Please info the name: ')
+age = input('Now the age: ')
+data = (name, age)
+
+cursor.execute(sql, (data))
+
+cursor.execute(
+    # f'INSERT INTO {TABLE_NAME} '
+    # '(name, age) VALUES ("Paulo", 19) '
+    sql, ('Antonio Brazino', 22)  # PARA EVITAR SQL INJECTION
+)
+connection.commit()
+
+data_with_dict = {
+    "this_variable_name_dont_import": "Rickson Jovem",
+    "that_too": 31923,
+}
+
+sql_with_dict = (
+    f'INSERT INTO {TABLE_NAME} '
+    '(name, age) '
+    'VALUES '
+    '(%(this_variable_name_dont_import)s, %(that_too)s) '
+)
+
+cursor.execute(sql_with_dict, data_with_dict)
 connection.commit()
 
 cursor.close()
